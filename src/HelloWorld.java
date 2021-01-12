27a2ccba7826067f0285bb3718daaa6d6fdf60cd
sujanshrestha <shrestha94.sujan@gmail.com>
2021-01-11 19:07:31 -0600

    commited major

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,5 +2,6 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World, my name is Renu");
         System.out.println("hello world, my name is sujan");
+        System.out.println("my major is computer science");
     }
 }
