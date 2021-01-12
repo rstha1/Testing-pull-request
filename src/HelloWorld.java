ee48831ab85098a6aa0389861b5c40c739dafae7
renushrestha <renustha18@gmail.com>
2021-01-11 19:08:53 -0600

    Added major

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,6 +2,8 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World, my name is Renu");
         System.out.println("hello world, my name is sujan");
+        System.out.println("My major is Nutrition");
+
         System.out.println("my major is computer science");
     }
 }
