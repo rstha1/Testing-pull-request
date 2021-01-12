83716a17df1542c02bc0f5de45acfb728b7f4998
renushrestha <renustha18@gmail.com>
2021-01-11 19:16:35 -0600

    Added age

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -5,6 +5,7 @@ public class HelloWorld {
         System.out.println("My major is Nutrition");
 
         System.out.println("my major is computer science");
+        System.out.println("I am 28 years old")
         System.out.println("my age is 26");
     }
 }
